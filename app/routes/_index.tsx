11e684f9580c60ffe227039b87feae84f1f37daf
saitama1899 <eric.lorien92@gmail.com>
2025-05-05 23:54:30 +0200

    feat: add loader to get real mails from db

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,9 +2,10 @@ import { LoaderFunctionArgs, json } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
 
 import { Mail } from "~/components/mail/mail"
-import { accounts, mails } from "~/data/mail-mock"
+import { accounts } from "~/data/mail-mock"
 
 import type { MetaFunction } from "@remix-run/node";
+import { db } from "models"
 
 export const meta: MetaFunction = () => {
 	return [
@@ -13,13 +14,12 @@ export const meta: MetaFunction = () => {
 	];
 };
 
-// 👇 Server-side loader para leer cookies
 export async function loader({ request }: LoaderFunctionArgs) {
   const cookieHeader = request.headers.get("cookie") || ""
   const cookies = Object.fromEntries(
-    cookieHeader.split(";").map((cookie) => {
-      const [key, ...rest] = cookie.trim().split("=")
-      return [key, decodeURIComponent(rest.join("="))]
+    cookieHeader.split(";").map((c) => {
+      const [k, ...v] = c.trim().split("=")
+      return [k, decodeURIComponent(v.join("="))]
     })
   )
 
@@ -31,26 +31,34 @@ export async function loader({ request }: LoaderFunctionArgs) {
     ? JSON.parse(cookies["react-resizable-panels:collapsed"])
     : undefined
 
-  return json({ defaultLayout, defaultCollapsed })
+  const emails = await db.Email.findAll({
+    include: {
+      model: db.Tag,
+			as: "tags",
+      through: { attributes: [] }, 
+    },
+    order: [["createdAt", "DESC"]],
+  })
+
+  const mails = emails.map((email) => ({
+    id: email.id.toString(),
+    name: email.email,
+    email: email.email,
+    subject: email.subject,
+    text: email.body,
+    date: email.createdAt,
+    read: email.read,
+    labels: (email.Tags || []).map((t) => t.name),
+  }))
+
+  return json({ defaultLayout, defaultCollapsed, mails })
 }
 
 export default function InboxPage() {
-  const { defaultLayout, defaultCollapsed } = useLoaderData<typeof loader>()
+  const { defaultLayout, defaultCollapsed, mails } = useLoaderData<typeof loader>()
 
   return (
     <>
-      <div className="md:hidden">
-        <img
-          src="/examples/mail-dark.png"
-          alt="Mail"
-          className="hidden dark:block w-full h-auto"
-        />
-        <img
-          src="/examples/mail-light.png"
-          alt="Mail"
-          className="block dark:hidden w-full h-auto"
-        />
-      </div>
       <div className="hidden flex-col md:flex">
         <Mail
           accounts={accounts}
